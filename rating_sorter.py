8d5ddd6da3d2ef9259e6bc3c4287a2820e9d4666
sevapova <slatipova99>
2025-07-03 22:44:25 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,14 @@
 
+import json
+
+with open("students.json", "r") as f:
+    students = json.load(f)
+
+students.sort(key=lambda s: int(s["score"]), reverse=True)
+
+for i, student in enumerate(students, start=1):
+    student["rank"] = i
+
+with open("rating.json", "w", encoding="utf-8") as f:
+    json.dump(students, f, ensure_ascii=False, indent=4)
+
